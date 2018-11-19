55414fbb72abb0e7d04f6f7fef1394ddf0e05b28
Felipe Rdguez Arias <ucifarias@gmail.com>
2018-11-19 13:50:18 -0500

    add pages of service...

--- src/AV/FrontendBundle/Controller/FrontendController.php
+++ src/AV/FrontendBundle/Controller/FrontendController.php
@@ -14,21 +14,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 class FrontendController extends Controller {
 
-    public function eventsAction(Request $request) {
-        $em = $this->getDoctrine()->getManager();
-        $cat = $em->getRepository(Entity::CATEGORIA)->find($this->getParameter('av.cat.eventos'));
-        $events = $em->getRepository(Entity::EVENTO)->findBy(['activo' => true, 'categoria' => $cat]);
-
-        if (count($events) > 6) {
-            shuffle($events);
-            $events = array_slice($events, 0, 6);
-        }
-
-        return $this->render('FrontendBundle:Frontend:events.html.twig', [
-            'events' => $events,
-        ]);
-    }
-
     public function eventDetailsRefAction(Request $request, $ref) {
         $em = $this->getDoctrine()->getManager();
         $event = $em->getRepository(Entity::EVENTO)->findOneBy(['ref' => $ref]);
@@ -39,6 +24,74 @@ class FrontendController extends Controller {
 
         return $this->render('FrontendBundle:Frontend:event-details-ref.html.twig', [
             'event' => $event
+        ]);
+    }
+
+    public function ministerioAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $faqs = $em->getRepository(Entity::FAQ)->findBy(['activo' => true]);
+        return $this->render('FrontendBundle:Frontend:ministerio.html.twig', [
+            'faqs' => $faqs
+        ]);
+    }
+
+    public function eventsAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $cat = $em->getRepository(Entity::CATEGORIA)->find($this->getParameter('av.cat.eventos'));
+        $events = $em->getRepository(Entity::EVENTO)->findBy(['activo' => true, 'categoria' => $cat]);
+
+        return $this->render('FrontendBundle:Frontend:events.html.twig', [
+            'events' => $events,
+            'textHeader' => $this->get('translator')->trans('f.eventos')
+        ]);
+    }
+
+    public function adoracionesAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $cat = $em->getRepository(Entity::CATEGORIA)->find($this->getParameter('av.cat.adoracion.alabanzas'));
+        $events = $em->getRepository(Entity::EVENTO)->findBy(['activo' => true, 'categoria' => $cat]);
+
+        return $this->render('FrontendBundle:Frontend:events.html.twig', [
+            'events' => $events,
+            'textHeader' => $this->get('translator')->trans('nav.adoracion.y.alabanzas')
+        ]);
+    }
+
+    public function momentosAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $cat = $em->getRepository(Entity::CATEGORIA)->find($this->getParameter('av.cat.momentos.sobrenaturales'));
+        $events = $em->getRepository(Entity::EVENTO)->findBy(['activo' => true, 'categoria' => $cat]);
+
+        return $this->render('FrontendBundle:Frontend:events.html.twig', [
+            'events' => $events,
+            'textHeader' => $this->get('translator')->trans('nav.momentos.supernaturales.y.testimonios')
+        ]);
+    }
+
+    public function ensenanzasAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $cat = $em->getRepository(Entity::CATEGORIA)->find($this->getParameter('av.cat.ensenanzas'));
+        $events = $em->getRepository(Entity::EVENTO)->findBy(['activo' => true, 'categoria' => $cat]);
+
+        return $this->render('FrontendBundle:Frontend:events.html.twig', [
+            'events' => $events,
+            'textHeader' => $this->get('translator')->trans('nav.ensenanzas')
+        ]);
+    }
+
+    public function trabajoComunitacioAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $cat = $em->getRepository(Entity::CATEGORIA)->find($this->getParameter('av.cat.trabajo.comunitario'));
+        $events = $em->getRepository(Entity::EVENTO)->findBy(['activo' => true, 'categoria' => $cat]);
+
+        return $this->render('FrontendBundle:Frontend:events.html.twig', [
+            'events' => $events,
+            'textHeader' => $this->get('translator')->trans('nav.trabajo.comunitario')
         ]);
     }
 
